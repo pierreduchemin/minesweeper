bec8ac8dbaef7ddbcf9d6bbb94fb2588a39c9f0a
Pierre Duchemin <pierre.duchemin@savoirfairelinux.com>
2020-07-02 19:40:42 -0400

    feat: better mine init
    will always avoid 1st clicked square

--- app/reducers/minesweeper.js
+++ app/reducers/minesweeper.js
@@ -5,44 +5,17 @@ import { create } from 'react-test-renderer';
 import { INIT, CLICK, FLAG } from '../actions/actionTypes';
 import { combineReducers } from 'redux';
 
-const init = () : Board => {
-  var a = [];
-  for (var x = 0; x < BOARD_SIZE; x++) {
-      a[x] = [];
-      for (var y = 0; y < BOARD_SIZE; y++) {
-          a[x].push(new Square(x, y, HIDDEN_TYPES.hidden, SHOWN_TYPES.empty));
-      }
-  }
-
-  var x, y, nMines = 0;
-  do {
-    x = Math.floor(Math.random() * BOARD_SIZE);
-    y = Math.floor(Math.random() * BOARD_SIZE);
-    if (a[x][y] === undefined || a[x][y].shownType === SHOWN_TYPES.empty) {
-      a[x][y].shownType = SHOWN_TYPES.mine;
-      nMines++;
-    }
-  } while (nMines < N_MINES);
-
-  var b = new Board(a);
-
-  b.getMines().forEach(function(m) {
-    b.getNeighbours(m).forEach(function(n) {
-      if (n.shownType !== SHOWN_TYPES.mine) {
-        n.value++;
-      }
-    });
-  });
-
-  return b;
-};
-
 const click = (board: Board, x: number, y: number): Board => {
   board.squares[x][y].hidden = false;
 
+  // initializing mines like this avoid the case where user 1st click is on a mine
+  if (!board.isMinesInit) {
+    board.initMines(x, y);
+  }
+
   // unhide all 0 value neighbours
   if (board.squares[x][y].value === 0) {
-    unhide(board, x, y);
+    board.unhide(x, y);
   }
 
   if (board.squares[x][y].shownType === SHOWN_TYPES.mine) {
@@ -54,18 +27,6 @@ const click = (board: Board, x: number, y: number): Board => {
   }
 
   return board;
-};
-
-const unhide = (board: Board, x: number, y: number) => {
-  const square = board.squares[x][y];
-  if (board !== undefined && square !== undefined) {
-    board.getNeighbours(square).filter(s => s.hidden).forEach(function(s) {
-      s.hidden = false;
-      if (s.value === 0 && s.shownType !== SHOWN_TYPES.mine) {
-        unhide(board, s.x, s.y);
-      }
-    });
-  }
 };
 
 const toggleFlag = (board: Board, x: number, y: number): Board => {
@@ -82,12 +43,12 @@ export type State = {
   board: Board,
 };
 
-const boardReducer = (state: State = { board: init() }, action: Object): Object => {
+const boardReducer = (state: State = { board: new Board() }, action: Object): Object => {
   switch (action.type) {
     case INIT:
       return {
         ...state,
-        board: init(),
+        board: new Board(),
       };
     case CLICK:
       var b = click(state.board, action.x, action.y);
